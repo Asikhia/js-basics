16b1c19da0299d2117b627ce7ef94074549053f0
Asikhia <asikhiatamara@gmail.com>
2026-02-13 12:08:56 +0100

    Add files via upload
    changing header in java script

---
+++ main.js
@@ -0,0 +1,6 @@
+	document.addEventListener('DOMContentLoaded', function() {
+		let header = document.querySelector('header.site-header h1');
+		if (header) {
+			header.textContent = 'My Portfolio — Asikhia Tamara';
+		}
+	});
